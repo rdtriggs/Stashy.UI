b33004ca19b50c444665938fc81771c4ca745761
Major Dutch <dutch@zypek.com>
2018-01-04 21:29:01 -0800

    core ui components

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,35 @@
+<template>
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item" v-for="(item, index) in list">
+      <span class="active" v-if="isLast(index)">{{ showName(item) }}</span>
+      <router-link :to="item" v-else>{{ showName(item) }}</router-link>
+    </li>
+  </ol>
+</template>
+
+<script>
+  export default {
+    props: {
+      list: {
+        type: Array,
+        required: true,
+        default: () => [],
+      },
+    },
+    methods: {
+      isLast(index) {
+        return index === this.list.length - 1;
+      },
+      showName(item) {
+        let ret = item;
+        if (item.meta && item.meta.label) {
+          ret = item.meta && item.meta.label;
+        }
+        if (item.name) {
+          ret = item.name;
+        }
+        return ret;
+      },
+    },
+  };
+</script>
