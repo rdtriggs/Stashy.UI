15f6d4a38ce65f9730fd852238eeb16caef26a8a
majordutch <354294+majordutch@users.noreply.github.com>
2018-05-02 08:37:24 -0700

    use value card

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-    <b-button size="sm" v-on:click="seedData();">Seed Data</b-button>
-    <div class="portfolio">
-      <AssetCard v-for="(item, index) in portfolio" v-bind:item="item" v-bind:index="index"
-                 v-bind:key="item.id"></AssetCard>
-    </div>
+  <div class="portfolio">
+    <!--<b-button size="sm" v-on:click="seedData();">Seed Data</b-button>-->
+    <ValueCard v-bind:portfolio="portfolio"></ValueCard>
+    <AssetCard v-for="(item, index) in portfolio.assets" v-bind:item="item" v-bind:index="index"
+               v-bind:key="item.id"></AssetCard>
   </div>
 </template>
 
 <script>
-  import AssetCard from '../components/AssetCard';
+  import ValueCard from '../components/portfolio/ValueCard';
+  import AssetCard from '../components/portfolio/AssetCard';
+  import { formatCurrency } from '../utils';
 
   export default {
     name: 'portfolio',
-    components: { AssetCard },
+    components: { ValueCard, AssetCard },
     data() {
       return {
         page: {
@@ -22,13 +23,13 @@
         },
       };
     },
-    head: {
-      title() {
-        return {
-          inner: this.page.title,
+    head() {
+      return {
+        title: {
+          inner: this.portfolio.value === undefined ? '0.00' : formatCurrency(this.portfolio.value, false, 'fiat'),
           separator: this.page.separator,
-        };
-      },
+        },
+      };
     },
     computed: {
       portfolio() {
@@ -37,7 +38,7 @@
     },
     methods: {
       seedData() {
-        this.$store.dispatch('addAsset', { id: 'neo', amount: 5050 });
+        this.$store.dispatch('addAsset', { id: 'neo', amount: 5100 });
         this.$store.dispatch('addAsset', { id: 'ethereum', amount: 233 });
         this.$store.dispatch('addAsset', { id: 'omisego', amount: 3772 });
         this.$store.dispatch('addAsset', { id: 'gas', amount: 2008 });
@@ -47,7 +48,17 @@
         this.$store.dispatch('addAsset', { id: 'bitcoin', amount: 3.165 });
         this.$store.dispatch('addAsset', { id: 'elastos', amount: 505 });
         this.$store.dispatch('addAsset', { id: 'ontology', amount: 826 });
+        this.$store.dispatch('addAsset', { id: 'aphelion', amount: 10000 });
+        this.$store.dispatch('addAsset', { id: 'effect-ai', amount: 13170 });
+        this.$store.dispatch('addAsset', { id: 'switcheo', amount: 20000 });
+        this.$store.dispatch('addAsset', { id: 'trinity-network-credit', amount: 5000 });
       },
+    },
+    mounted() {
+      const self = this;
+      this.$nextTick(() => {
+        self.$emit('updateHead');
+      });
     },
   };
 </script>
