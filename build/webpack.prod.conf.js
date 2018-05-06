31b90069de71f7b993ecb19faf7369a88b7f3d1c
majordutch <354294+majordutch@users.noreply.github.com>
2018-05-06 00:05:30 -0700

    copy robots.txt in build process

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -93,6 +93,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../robots.txt'),
+        to: config.build.assetsRoot
       }
     ])
   ]
