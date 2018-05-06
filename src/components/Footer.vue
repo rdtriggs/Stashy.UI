622c8a0737b255570d07298c4212a7070e5c3ae0
majordutch <354294+majordutch@users.noreply.github.com>
2018-05-06 15:40:26 -0700

    fix footer in small mode

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="app-footer">
-    <span>&copy; 2018 Zypek. All rights reserved.</span>
-    <span class="ml-auto">Data provided by <a href="https://coinmarketcap.com/" target="_blank">CoinMarketCap</a></span>
+    <div class="copyright">&copy; 2018 Zypek. All rights reserved.</div>
+    <div class="attribution">Data provided by <a href="https://coinmarketcap.com/"
+                                                 target="_blank">CoinMarketCap</a></div>
   </footer>
 </template>
 
