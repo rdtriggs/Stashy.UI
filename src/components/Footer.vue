efbccf8d6dbfa36c4e3b519bcd54bc69676efef9
majordutch <354294+majordutch@users.noreply.github.com>
2018-11-16 20:39:44 -0800

    update link for data partner

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,8 @@
     <div class="copyright">Copyright &copy; 2018 Stashy. Released under the <a
       href="https://opensource.org/licenses/mit-license.php" target="_blank">MIT License</a>.
     </div>
-    <div class="attribution">Some data provided by <a href="https://coinmarketcap.com/"
-                                                      target="_blank">CoinMarketCap</a></div>
+    <div class="attribution">Some data provided by <a href="https://www.coingecko.com/en"
+                                                      target="_blank">CoinGecko</a></div>
   </footer>
 </template>
 
